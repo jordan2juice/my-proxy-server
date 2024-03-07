984da479dae25981a7c7b68e937e752351706441
jordan toujouse <jtoujo1@gmail.com>
2024-03-06 21:39:14 -0600

    create backend

---
+++ index.js
@@ -0,0 +1,40 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Enable all CORS requests
+app.use(cors());
+
+app.get('/search', async (req, res) => {
+    try {
+        // Extract query parameters from client request
+        const { query } = req.query;
+
+        // Define Spoonacular API URL
+        const spoonacularAPI = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${process.env.SPOONACULAR_API_KEY}&query=${query}`;
+
+        // Forward request to Spoonacular API
+        const response = await axios.get(spoonacularAPI);
+
+        // Send back the data received from Spoonacular API to the client
+        res.json(response.data);
+    } catch (error) {
+        // Handle errors
+        console.error('Error while calling Spoonacular API:', error);
+        res.status(error.response?.status || 500).json({ message: error.message });
+    }
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+
+
+
+
+
